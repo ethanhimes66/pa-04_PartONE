0a8a1a4b8eaaae1b74202cf906a220120831373d
James H. Handlon <handlojh@dukes.jmu.edu>
2024-11-03 17:09:13 -0500

    crypto partial

--- myCrypto.c
+++ myCrypto.c
@@ -13,9 +13,284 @@ Submitted on:
 
 #include "myCrypto.h"
 
+//***********************************************************************
+// pLAB-01
+//***********************************************************************
+
+void handleErrors( char *msg)
+{
+    fprintf( stderr , "\n%s\n" , msg ) ;
+    ERR_print_errors_fp(stderr);
+    exit(-1);
+}
+
+
+unsigned encrypt( uint8_t *pPlainText, unsigned plainText_len, 
+             const uint8_t *key, const uint8_t *iv, uint8_t *pCipherText)
+{
+  int status;
+  unsigned len=0, encryptedLen=0;
+
+  EVP_CIPHER_CTX  *ctx = EVP_CIPHER_CTX_new();
+  if( !ctx )
+  {
+    handleErrors("encrypt: failed to creat CTX");
+  }
+
+  status = EVP_EncryptInit_ex( ctx, ALGORITHM(), NULL, key, iv);
+  if( status != 1 )
+  {
+    handleErrors("encrypt: failed to EncryptInit_ex");
+  }
+
+
+  status = EVP_EncryptUpdate(ctx, pCipherText, &len, pPlainText, plainText_len);
+  if( status != 1 )
+  {
+    handleErrors("encrypt: failed to EncryptUpdate");
+  }
+  encryptedLen += len;
+
+  pCipherText += len;
+
+  status = EVP_EncryptFinal_ex( ctx, pCipherText, &len );
+  if( status != 1 )
+  {
+    handleErrors("encrypt: failed to EncryptFinal_ex");
+  }
+  encryptedLen += len;
+
+  EVP_CIPHER_CTX_free(ctx);
+
+  return encryptedLen;
+}
+
+unsigned decrypt( uint8_t *pCipherText, unsigned cipherText_len, 
+             const uint8_t *key, const uint8_t *iv, uint8_t *pDecryptedText)
+{
+  int status;
+  unsigned len=0, decryptedLen=0;
+
+  EVP_CIPHER_CTX  *ctx = EVP_CIPHER_CTX_new();
+  if( !ctx )
+  {
+    handleErrors("decrypt: failed to creat CTX");
+  }
+
+  status = EVP_DecryptInit_ex( ctx, ALGORITHM(), NULL, key, iv);
+  if( status != 1 )
+  {
+    handleErrors("decrypt: failed to DecryptInit_ex");
+  }
+
+  status = EVP_DecryptUpdate(ctx, pDecryptedText, &len, pCipherText, cipherText_len);
+  if( status != 1 )
+  {
+    handleErrors("decrypt: failed to DecryptUpdate");
+  }
+  decryptedLen += len;
+
+  pDecryptedText += len;
+
+  status = EVP_DecryptFinal_ex( ctx, pDecryptedText, &len );
+  if( status != 1 )
+  {
+    handleErrors("decrypt: failed to DecryptFinal_ex");
+  }
+  decryptedLen += len;
+
+  EVP_CIPHER_CTX_free(ctx);
+
+  return decryptedLen;
+}
+
+EVP_PKEY *getRSAfromFile(char * filename, int public)
+{
+    FILE * fp = fopen(filename,"rb");
+    if (fp == NULL)
+    {
+        fprintf( stderr , "getRSAfromFile: Unable to open RSA key file %s \n",filename);
+        return NULL;    
+    }
+
+    EVP_PKEY *key = EVP_PKEY_new() ;
+    if ( public )
+        key = PEM_read_PUBKEY( fp, &key , NULL , NULL );
+    else
+        key = PEM_read_PrivateKey( fp , &key , NULL , NULL );
+ 
+    fclose( fp );
+
+    return key;
+}
+
+
+//***********************************************************************
+// PA-02
+//***********************************************************************
+// Sign the 'inData' array into the 'sig' array using the private 'privKey'
+// 'inLen' is the size of the input array in bytes.
+// the '*sig' pointer will be allocated memory large enough to store the signature
+// report the actual length in bytes of the result in 'sigLen' 
 //
-//  ALL YOUR  CODE FORM  PREVIOUS PAs  and pLABs
-//  MUST be Here
+// Returns: 
+//    1 on success, or 0 on ANY REASON OF FAILURE
+
+int privKeySign( uint8_t **sig , size_t *sigLen , EVP_PKEY  *privKey , 
+                 uint8_t *inData , size_t inLen ) 
+{
+    // Guard against incoming NULL pointers
+    if ( !sig || !privKey || !inData )
+    {
+        printf(  "\n******* pkeySign received some NULL pointers\n" ); 
+        return 0 ; 
+    }
+
+    // Create and Initialize a context for RSA private-key signing
+    EVP_PKEY_CTX *ctx =  EVP_PKEY_CTX_new(privKey, NULL);
+    if ( !ctx )
+    {
+      handleErrors("privKeySign: failed to creat CTX");
+      EVP_PKEY_CTX_free( ctx );
+      return 0;
+    }
+
+    if (EVP_PKEY_sign_init(ctx) != 1)
+    {
+      handleErrors("privKeySign: failed to EVP_PKEY_sign_init");
+      EVP_PKEY_CTX_free( ctx );
+      return 0;
+    }
+
+    // Determine how big the size of the signature could be
+    size_t cipherLen ; 
+    if (EVP_PKEY_sign(ctx, NULL, &cipherLen, inData, inLen) <= 0)
+    {
+      handleErrors("privKeySign: failed to get size of signature");
+      EVP_PKEY_CTX_free( ctx );
+      return 0;
+    }
+
+    // Next allocate memory for the ciphertext
+    *sig = malloc(cipherLen);
+    if (!(*sig))
+    {
+      handleErrors("privKeySign: failed to allocate memory");
+      EVP_PKEY_CTX_free( ctx );
+      return 0;
+    }
+
+    // Now, actually sign the inData using EVP_PKEY_sign( )
+    if (EVP_PKEY_sign(ctx, *sig, sigLen, inData, inLen) <= 0)
+    {
+      handleErrors("privKeySign: failed to get size of signature");
+      EVP_PKEY_CTX_free( ctx );
+      return 0;
+    }
+
+    // All is good
+    EVP_PKEY_CTX_free( ctx );     // remember to do this if any failure is encountered above
+
+    return 1;
+}
+
+//-----------------------------------------------------------------------------
+// Verify that the provided signature in 'sig' when decrypted using 'pubKey' 
+// matches the data in 'data'
+// Returns 1 if they match, 0 otherwise
+
+int pubKeyVerify( uint8_t *sig , size_t sigLen , EVP_PKEY  *pubKey 
+           , uint8_t *data , size_t dataLen ) 
+{
+    // Guard against incoming NULL pointers
+    if ( !sig ||  !pubKey  ||  !data  )
+    {
+        printf(  "\n******* pkeySign received some NULL pointers\n" ); 
+        return 0 ; 
+    }
+
+    // Create and Initialize a context for RSA public-key signature verification
+    EVP_PKEY_CTX *ctx = EVP_PKEY_CTX_new(pubKey, NULL);
+    if( !ctx )
+    {
+      handleErrors("pubKeyVerify: failed to creat CTX");
+      EVP_PKEY_CTX_free( ctx );
+      return 0;
+    }
+    
+    if (EVP_PKEY_verify_init(ctx) != 1)
+    {
+      handleErrors("privKeySign: failed to EVP_PKEY_sign_init");
+      EVP_PKEY_CTX_free( ctx );
+      return 0;
+    }
+
+    // Verify the signature vs the incoming data using this context
+    int decision = EVP_PKEY_verify(ctx, sig, sigLen, data, dataLen) ;
+
+    //  free any dynamically-allocated objects 
+    EVP_PKEY_CTX_free( ctx );
+
+    return decision ;
+
+}
+
+//-----------------------------------------------------------------------------
+
+
+size_t fileDigest( int fd_in , int fd_out , uint8_t *digest )
+// Read all the incoming data stream from the 'fd_in' file descriptor
+// Apply the HASH_ALGORITHM() to compute the hash value of this incoming data into the array 'digest'
+// If the file descriptor 'fd_out' is > 0, also write a copy of the incoming data stream file to 'fd_out'
+// Returns actual size in bytes of the computed digest
+{
+    EVP_MD_CTX *mdCtx ;
+    size_t nBytes ;
+    unsigned int  mdLen ;
+
+	  // Use EVP_MD_CTX_create() to create new hashing context    
+    mdCtx = EVP_MD_CTX_create();
+    
+    // Initialize the context using EVP_DigestInit() so that it deploys 
+	  // the HASH_ALGORITHM() hashing function 
+    if (EVP_DigestInit(mdCtx, HASH_ALGORITHM()) != 1)
+    {
+      EVP_MD_CTX_destroy(mdCtx);
+      return 0;
+    }
+
+    uint8_t buffer[4096];
+
+    while ( (nBytes = read(fd_in, buffer, 4096)) > 0 )   // Loop until end-of input file
+    {
+        // Read a chunk of input from fd_in. Exit the loop when End-of-File is reached
+        if (EVP_DigestUpdate(mdCtx, buffer, nBytes) != 1)
+        {
+          EVP_MD_CTX_destroy(mdCtx);
+          return 0;
+        }
+
+        // VP_DigestUpdate( )
+        
+        // if ( fd_out > 0 ) send the above chunk of data to fd_out
+        if (fd_out > 0)
+        {
+          write(fd_out, buffer, nBytes);
+        }
+    }
+
+    // EVP_DigestFinal( )
+    if (EVP_DigestFinal(mdCtx, digest, &mdLen) != 1)
+    {
+      EVP_MD_CTX_destroy(mdCtx);
+      return 0;
+    }
+    
+    // EVP_MD_CTX_destroy( );
+    EVP_MD_CTX_destroy(mdCtx);
+
+    return mdLen ;
+}
 
 //***********************************************************************
 // PA-04  Part  One
